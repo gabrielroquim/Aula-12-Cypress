1e839d82954bfb326cacac1a1464742f14bc3812
Gabriel Roquim <gabrielroquim@hotmail.com>
2022-01-20 15:59:04 -0300

    Alterações Precadastro

--- cypress/integration/enderecos.spec.js
+++ cypress/integration/enderecos.spec.js
@@ -1,46 +1,18 @@
-/// <reference types="cypress"/>
-var faker = require('faker-br');
-const Faker = require('faker-br/lib');
+/// <reference types="cypress" />
 
-describe('Funcionalidade cadastro de endereços', () => {
+describe('Funcionalidade Endereços -  Faturamento e Entregas', () => {
 
+    // Quando visitar minha conta, sistema faz o login só para depois iniciar os testes
     beforeEach(() => {
         cy.visit('minha-conta')
+        cy.fixture('perfil').then(dados =>{
+            cy.login(dados.usuario, dados.senha)
+        })
 
     });
 
-    it('Deve fazer login com sucesso', () => {
-
-        cy.get('#username').type('luigi@nintendo.com')
-        cy.get('#password').type('mario@mario.!')
-        cy.get('.woocommerce-form > .button').click()
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, luigi')
-
-        cy.get('.woocommerce-MyAccount-navigation-link--edit-address > a').click()
-        cy.get('h2').should('contain', 'My Addresses')
-
-        cy.get(':nth-child(1) > .title > .edit').click()
-        cy.get('#billing_first_name').clear().type(faker.name.firstName())
-        cy.get('#billing_last_name').clear().type(faker.name.lastName())
-
-        cy.get('#select2-billing_country-container').click()
-        cy.contains('Brasil').click({ force: true })
-
-        cy.get('#billing_address_1').clear().type(faker.address.streetName())
-        cy.get('#billing_city').clear().type(faker.address.city())
-        
-
-        cy.get('#select2-billing_state-container').click()
-        cy.contains('São Paulo').click({force: true})
-        
-        cy.get('#billing_postcode').clear().type(faker.address.zipCodeValid())
-        cy.get('#billing_phone').clear().type(faker.phone.phoneNumber())
-        cy.get(':nth-child(2) > .button').click()
-
-        cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso.')
-        cy.get('.woocommerce-MyAccount-navigation-link--customer-logout > a').click()
-        
-
+    it('Deve fazer cadastro de faturamento com sucesso', () => {
+            
 
     })
 
