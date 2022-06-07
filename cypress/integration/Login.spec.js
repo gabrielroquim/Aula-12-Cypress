0712cbe1d027e82c11f62c0ce2b9478ef5f8f7ab
Gabriel Roquim <gabrielroquim@hotmail.com>
2022-06-07 15:04:17 -0300

    testeOk

--- cypress/integration/Login.spec.js
+++ cypress/integration/Login.spec.js
@@ -13,7 +13,7 @@ context('Funcionalidade Login', () => {
     });
 
     it('Deve fazer login com sucesso', () => {
-        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#username').type('aluno_ebac@teste.com')       
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
 
@@ -39,6 +39,7 @@ context('Funcionalidade Login', () => {
             cy.get('.woocommerce-form > .button').click()
 
             cy.get('.page-title').should('contain', 'Minha conta')
+            cy.get('.page-title')
 
         })
 
